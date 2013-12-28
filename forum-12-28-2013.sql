9619c86feda9eac515b9e3220ea3c4403a97210e
paullewallencom <paul.lewallen@gmail.com>
2013-12-28 07:48:06 -0800

    SQL "Dummy data"

--- forum-12-28-2013.sql
+++ forum-12-28-2013.sql
@@ -35,7 +35,15 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Movies'),
+(2, 'Documentaries');
 
 -- --------------------------------------------------------
 
@@ -49,7 +57,15 @@ CREATE TABLE IF NOT EXISTS `forums` (
   `name` varchar(30) NOT NULL,
   `description` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `forums`
+--
+
+INSERT INTO `forums` (`id`, `cat_id`, `name`, `description`) VALUES
+(1, 1, 'Comedy', 'Films that are hilarious'),
+(2, 1, 'Horror', 'Movies to hide behind the couch');
 
 -- --------------------------------------------------------
 
@@ -65,7 +81,15 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `subject` varchar(100) NOT NULL,
   `body` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `date`, `user_id`, `topic_id`, `subject`, `body`) VALUES
+(1, '2013-12-28 07:44:14', 1, 1, 'Classic comedy', 'Which black and white coomedies do you like?'),
+(2, '2013-12-28 07:45:05', 1, 2, 'Fave comedy actor/actress', 'Who is your fave funny man/women?');
 
 -- --------------------------------------------------------
 
@@ -80,7 +104,15 @@ CREATE TABLE IF NOT EXISTS `topics` (
   `forum_id` tinyint(4) NOT NULL,
   `subject` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `topics`
+--
+
+INSERT INTO `topics` (`id`, `date`, `user_id`, `forum_id`, `subject`) VALUES
+(1, '2013-12-28 07:41:15', 1, 1, 'Classic comedy'),
+(2, '2013-12-28 07:42:18', 1, 1, 'Fave comedy actor/actress');
 
 -- --------------------------------------------------------
 
@@ -96,7 +128,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `verifystring` varchar(20) NOT NULL,
   `active` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `verifystring`, `active`) VALUES
+(1, 'Johnsmith', 'Password', 'john@foo.com', '', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
