b14991bb9e40c92b731434d4e5eda81d6190afbd
paullewallencom <paul.lewallen@gmail.com>
2013-12-28 08:42:33 -0800

    Basic functionality, continue building.

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+    $dbhost = "localhost";
+
+    $dbuser = "root";
+
+    $dbpassword = "";
+
+    $dbdatabase = "forum-12-28-2013";
+
+    // Add the name of the forums below
+
+    $config_forumsname = "CineaForums";
+
+    // Add your name below
+
+    $config_admin = "Paul Lewallen";
+
+    $config_adminemail = "paul AT paullewallen DOT com";
+
+    // Add the location of your forums below
+
+    $config_basedir = "http://127.0.0.1/_SandBox/PHP-discussion-forum/";
+
+?>
---
+++ footer.php
@@ -0,0 +1,7 @@
+</div>
+<div id="footer">
+    &copy; 2005 <?php echo "<a href='mailto:"
+            . $config_adminemail . "'>" .$config_admin . "</a>"; ?>
+</div>
+</body>
+</html>
